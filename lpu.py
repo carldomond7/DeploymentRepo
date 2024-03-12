dd1a2173de5342a2b87da72f63d31a9d29159131
carldomond7 <124755395+carldomond7@users.noreply.github.com>
2024-03-11 20:11:34 -0700

    Update lpu.py
    Data provided was unable to be prcoessed

--- lpu.py
+++ lpu.py
@@ -9,11 +9,7 @@ from langchain_groq import ChatGroq  # Update with the correct import based on y
 groq_api_key = os.getenv("GROQ_API_KEY")
 
 class UserRequest(BaseModel):
-    Category: str
-    SubCategory: str
-    Title: str
-    Objective: str
-    Description: str
+    
     DetailedPrompt: str
     Input1: str
     Input2: str
@@ -31,12 +27,7 @@ async def root():
 @app.post("/route/")
 async def process_request(request: UserRequest):
     llm = ChatGroq(groq_api_key=groq_api_key, model_name='mixtral-8x7b-32768')
-
-    category = request.Category
-    subcategory = request.SubCategory
-    title = request.Title
-    objective = request.Objective
-    description = request.Description
+    
     detailedprompt = request.DetailedPrompt
     input1 = request.Input1
     input2 = request.Input2
@@ -45,12 +36,7 @@ async def process_request(request: UserRequest):
     input5 = request.Input5
 
     prompt_template = """ 
-    Using the context below answer the question to the best of your ability
-    {category}
-    {subcategory}
-    {title}
-    {objective}
-    {description}
+    Using the context below answer the question to the best of your ability   
     {input1}
     {input2}
     {input3}
@@ -61,7 +47,7 @@ async def process_request(request: UserRequest):
 
 # Define the prompt structure
     prompt = PromptTemplate(
-    input_variables=["detailedprompt", "category", "subcategory", "title", "objective", "description", "input1", "input2", "input3", "input4", "input5"],
+    input_variables=["detailedprompt", "input1", "input2", "input3", "input4", "input5"],
     template=prompt_template,
 )
 
@@ -69,7 +55,7 @@ async def process_request(request: UserRequest):
     llm_chain = LLMChain(llm=llm, prompt=prompt)
 
     # Pass the context and question to the Langchain chain
-    result_chain = llm_chain.invoke({"detailedprompt": detailedprompt, "category": category, "subcategory": subcategory, "title": title, "objective": objective, "description": description, "input1": input1, "input2": input2, "input3": input3, "input4": input4, "input5": input5})
+    result_chain = llm_chain.invoke({"detailedprompt": detailedprompt, "input1": input1, "input2": input2, "input3": input3, "input4": input4, "input5": input5})
     return result_chain
 
 if __name__ == "__main__":
